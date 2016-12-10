4bd8a05e5d0e8f80a043b0cc11165115bdf81356
Jeff Wu <crazy.alu@outlook.com>
2016-12-10 15:24:32 +0800

    串接 table-detail 假資料

--- project/src/app/table-detail/table-detail.component.ts
+++ project/src/app/table-detail/table-detail.component.ts
@@ -1,4 +1,6 @@
+import { OrderService } from './../order.service';
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-table-detail',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table-detail.component.css']
 })
 export class TableDetailComponent implements OnInit {
-
-  constructor() { }
+  tableId;
+  tableInfo;
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _orderService: OrderService
+  ) {
+    this._route.params.subscribe(param => {
+      console.log('tableID is ', param);
+      this.tableId = param['id'];
+    })
+  }
 
   ngOnInit() {
+    this.tableInfo = this._orderService.mockData[this.tableId];
   }
 
 }
