ad85b742c34a5f963371f80d4004eaf1eb17f5f1
Jeff Wu <crazy.alu@outlook.com>
2016-12-10 14:57:49 +0800

    建立假資料，實作 ngFor 輸出 tables

--- project/src/app/tables/tables.component.ts
+++ project/src/app/tables/tables.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tables.component.css']
 })
 export class TablesComponent implements OnInit {
-
+  mockData = [{
+    orderTime: '12:00',
+    person: 2,
+    orderPrice: 300,
+  },
+  {
+    orderTime: '12:00',
+    person: 4,
+    orderPrice: 320,
+  },
+  {
+    orderTime: '12:00',
+    person: 6,
+    orderPrice: 3200,
+  }]
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
